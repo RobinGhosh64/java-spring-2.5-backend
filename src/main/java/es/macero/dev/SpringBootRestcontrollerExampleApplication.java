e66eff1b41808a53a71883d32350f4ef4c0a5a02
RobinGhosh64 <rrrghosh@yahoo.com>
2025-02-04 18:02:36 -0500

    Update SpringBootRestcontrollerExampleApplication.java

--- src/main/java/es/macero/dev/SpringBootRestcontrollerExampleApplication.java
+++ src/main/java/es/macero/dev/SpringBootRestcontrollerExampleApplication.java
@@ -3,20 +3,20 @@ package es.macero.dev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
+//@EnableSwagger2
 public class SpringBootRestcontrollerExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestcontrollerExampleApplication.class, args);
 	}
-
+/*
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
@@ -25,5 +25,5 @@ public class SpringBootRestcontrollerExampleApplication {
 				.paths(PathSelectors.regex("/spanish-greeting.*"))
 				.build();
 	}
-
+*/
 }
